ae80d49d62d90e488247b5f52d15f9524478e576
Harvs04 <hamartinez@up.edu.ph>
2024-08-24 14:36:22 +0800

    added params to reporting.blade.php

--- org-application/resources/views/Applicant/reporting.blade.php
+++ org-application/resources/views/Applicant/reporting.blade.php
@@ -2,7 +2,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-                <x-applicant.reporting :startdate="$start_date" :enddate="$end_date" :developers="$developers"/>
+                <x-applicant.reporting :startdate="$start_date" :enddate="$end_date" :developers="$developers" :totalreportingcount="$total_reporting_count" :accomplishedreportingcount="$accomplished_reporting_count"  />
             </div>
         </div>
     </div>
