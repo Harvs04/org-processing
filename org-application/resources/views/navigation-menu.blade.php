400256fc8167c222f39d9d61e6d20f8f0ab2cf25
Harvs04 <hamartinez@up.edu.ph>
2024-08-25 13:53:45 +0800

    adjusted navigation pos

--- org-application/resources/views/navigation-menu.blade.php
+++ org-application/resources/views/navigation-menu.blade.php
@@ -1,13 +1,23 @@
-<nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
+<nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700 sticky top-0 w-full z-50">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-mark class="block h-9 w-auto" />
-                    </a>
+                    @if (Auth::user()->role === "Admin")
+                        <a href="{{ route('dashboard') }}">
+                            <x-application-mark class="block h-9 w-auto" />
+                        </a>
+                    @elseif (Auth::user()->role === "Developer")
+                        <a href="{{ route("/") }}">
+                            <x-application-mark class="block h-9 w-auto" />
+                        </a>
+                    @elseif (Auth::user()->role === "Applicant")
+                        <a href="{{ route('applicant-dashboard') }}">
+                            <x-application-mark class="block h-9 w-auto" />
+                        </a>
+                    @endif
                 </div>
 
                 <!-- Navigation Links -->
